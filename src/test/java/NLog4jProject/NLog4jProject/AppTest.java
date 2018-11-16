f3289b09875c81a0762f55ad3d9992e43376338e
nmorise <nperi3d@hotmail.com>
2018-11-15 21:05:48 -0800

    1115 commit

---
+++ src/test/java/NLog4jProject/NLog4jProject/AppTest.java
@@ -0,0 +1,20 @@
+package NLog4jProject.NLog4jProject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
